9cbd0b2ed059393df3d85db127834ef0aad7d355
kimhobeen <sksjsksh32@naver.com>
2023-08-06 02:51:12 +0900

    update workflow

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,37 @@ import './App.css';
 
 function App() {
   const [data, setData] = useState(null);
+  const [DBdata, setDBData] = useState(null);
 
   const fetchData = () => {
-    fetch("http://localhost:8080/test")
+    fetch("http://ec2-3-34-138-44.ap-northeast-2.compute.amazonaws.com/test")
       .then((res) => res.text())  // Change this line
       .then((data) => setData(data))
       .catch((error) => console.log(error));
   };
 
+  const fetchDBData = () => {
+    fetch("http://ec2-3-34-138-44.ap-northeast-2.compute.amazonaws.com/test/name1")
+      .then((res) => res.text())  // Change this line
+      .then((data) => setDBData(data))
+      .catch((error) => console.log(error));
+  };
+
   return (
+    <>
     <div>
       <button onClick={fetchData}>Fetch Data</button>
       {data && 
       <pre>data : {data}</pre>
       }
     </div>
+    <div>
+    <button onClick={fetchDBData}>Fetch DBdata</button>
+    {data && 
+    <pre>DBdata : {DBdata}</pre>
+    }
+  </div>
+  </>
   );
 }
 
